9bdc0dfb924ef05dbe867b82328b2be0732ed746
Pedro Fraca <pedro.fraca@adidas.com>
2023-02-19 19:18:21 +0100

    Adding open metrics support

--- src/main/kotlin/io/github/pedrofraca/GeneratorResource.kt
+++ src/main/kotlin/io/github/pedrofraca/GeneratorResource.kt
@@ -3,6 +3,9 @@ package io.github.pedrofraca
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
+import org.eclipse.microprofile.metrics.MetricUnits
+import org.eclipse.microprofile.metrics.annotation.Counted
+import org.eclipse.microprofile.metrics.annotation.Timed
 import java.io.ByteArrayOutputStream
 import java.util.*
 import javax.ws.rs.Consumes
@@ -17,6 +20,8 @@ class GeneratorResource {
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
+    @Counted(description = "How many qrs get generated", absolute = true, name = "countQR")
+    @Timed(name = "qr_generation", description = "A measure of how long it takes to generate a QR", unit = MetricUnits.MILLISECONDS)
     fun generate(request : GenerateQRRequest)  : String {
 
         val writer = QRCodeWriter()
